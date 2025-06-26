2394da18fa6f3454080968385498651bbd7e35d7
SantiGauna <sgaunamartinez@gmail.com>
2025-06-26 11:59:37 -0300

    Modificaciones en el workflows y playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'Webkit',
+      use: { browserName: 'webkit' },
+    },
+  ],
   use: {
     baseURL: 'https://tuapp.com',
     headless: true,
